259f7dbf2b352acdcbb605e83e19b6b35506153b
Nathan Tastet <nathan.tastet69@gmail.Com>
2025-01-06 23:19:02 +0100

    commit de la bdd

--- SAE_BDD.sql
+++ SAE_BDD.sql
@@ -5,7 +5,8 @@
 -- Hôte : localhost
 -- Généré le : lun. 06 jan. 2025 à 23:16
 -- Version du serveur : 8.0.40-0ubuntu0.22.04.1
--- Version de PHP : 8.1.2-1ubuntu2.20
+-- Version de PHP : 8.1.2-1ubuntu2.21
+--
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
